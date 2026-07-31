f4d665b29acbde7c4494814ad407a450a794535c
Kirill Siukhin <kirillsiukhin@proton.me>
2026-07-31 20:08:54 +0500

    feat: update website design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zorreth.com',
+  vite: {
+    plugins: [tailwindcss()],
+  },
   fonts: [
     {
       provider: fontProviders.fontsource(),
