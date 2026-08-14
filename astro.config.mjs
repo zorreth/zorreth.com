7546a00e10f6206343f8a38af0beef5872fd544c
Kirill Siukhin <kirillsiukhin@proton.me>
2026-08-15 01:09:59 +0500

    feat: replace font, add footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,8 @@ export default defineConfig({
   fonts: [
     {
       provider: fontProviders.fontsource(),
-      name: 'Fira Code',
-      cssVariable: '--font-fira-code',
-      weights: [400, 600],
+      name: 'DM Sans',
+      cssVariable: '--font-dm-sans',
     },
   ],
 });
